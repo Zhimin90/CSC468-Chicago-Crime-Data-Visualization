b8124cda7aa52f171002493bef083042ea97657c
Khoa Nguyen <Khoa@Khoas-MacBook-Pro.local>
2020-05-23 00:58:59 -0500

    added toggle buttons for layers and decreased size of slider window

--- D3_Visualization/mapbox.js
+++ D3_Visualization/mapbox.js
@@ -191,4 +191,40 @@ function runMapbox(map,url) {
     map.addControl(new mapboxgl.NavigationControl());
     map.dragRotate.disable();
     map.doubleClickZoom.disable();
+
+    //Below is the code for the toggle on/off buttons for the different categories: heatmap, crimes-point, and crime category labels
+    //documentation via https://docs.mapbox.com/mapbox-gl-js/example/toggle-layers/
+    var toggleableLayerIds = ['crimes-heat','crimes-point','crime-category-labels'];     // enumerate ids of the layers
+    
+    // set up the corresponding toggle button for each layer
+    for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+    
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id; 
+    
+    link.onclick = function(e) {
+    var clickedLayer = this.textContent;
+    e.preventDefault();
+    e.stopPropagation();
+    
+    var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+    
+    // toggle layer visibility by changing the layout object's visibility property
+    if (visibility === 'visible') {
+    map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+    this.className = '';
+    } else {
+    this.className = 'active';
+    map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+    }
+    };
+    
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+    } 
+
+
 }
